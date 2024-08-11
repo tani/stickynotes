08b74677fde08d87b8cd44bc3180226e3a80658b
Masaya Taniguchi <ta2gch@gmail.com>
2024-08-12 04:36:56 +0900

    feat: add favicon and update stylesheet link
    - Added a favicon link to the head section of the document.
    - Updated the stylesheet link to use the non-minified version.

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -9,7 +9,8 @@ export default jsxRenderer(({ children, title }) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/modern-normalize@2/modern-normalize.min.css" />
+        <link rel="icon" href="https://cdn.jsdelivr.net/npm/@twemoji/svg@15/1f4d1.svg" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/modern-normalize@2/modern-normalize.css" />
         <link rel="icon" href="/favicon.ico" />
         <Script src="/app/client.ts" async />
         <Style />
