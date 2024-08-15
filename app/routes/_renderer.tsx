ceec22b185d9c4aa54c357f649619dd57f811e8c
Masaya Taniguchi <ta2gch@gmail.com>
2024-08-16 02:01:35 +0900

    feat: update renderer and dependencies
    - Added `Link` import from `honox/server` in `_renderer.tsx`
    - Replaced stylesheet link with `Link` component in `_renderer.tsx`
    - Updated `package.json` to include `modern-normalize` dependency

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,6 +1,6 @@
 import { Style } from 'hono/css'
 import { jsxRenderer } from 'hono/jsx-renderer'
-import { Script } from 'honox/server'
+import { Script, Link } from 'honox/server'
 
 export default jsxRenderer(({ children, title }) => {
   return (
@@ -10,8 +10,7 @@ export default jsxRenderer(({ children, title }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
         <link rel="icon" href="https://cdn.jsdelivr.net/npm/@twemoji/svg@15/1f4d1.svg" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/modern-normalize@2/modern-normalize.css" />
-        <link rel="icon" href="/favicon.ico" />
+        <Link href="/app/client.css" rel="stylesheet" />
         <Script src="/app/client.ts" async />
         <Style />
       </head>
